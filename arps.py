b723d9b74f95fb055dc55d696f2274714b92adc5
Peter Rogers <peterjrogers@hotmail.com>
2014-02-10 03:19:06 +0000

    Update arps.py

--- arps.py
+++ arps.py
@@ -1,6 +1,6 @@
-import pickle, sys
+import pickle, sys, os
 from mac import Mac
-from mnet import Mnet
+from mnet2 import Mnet
 
 
 class Arp(Mnet, Mac):
@@ -11,7 +11,7 @@ class Arp(Mnet, Mac):
         """
         Arp testing tools
         
-        # dict format
+        # new dict format
         # arp_dict = {}
         # arp_dict['b0096'] = {}
         # arp_dict['b0096']['Vlan200'] = {}
@@ -25,26 +25,39 @@ class Arp(Mnet, Mac):
         # arp_dict['b0096']['Vlan200']['10.67.6.81_0000.0c07.acc9']['date'] = '2013-04-08'
         # arp_dict['b0096']['Vlan200']['10.67.6.81_0000.0c07.acc9']['time'] = '00:14:19'
         
-        (c) 2012, 2013 Intelligent Planet Ltd
+        new nexus format
+        Address         Age       MAC Address     Interface
+        172.19.167.5    00:00:57  0001.d7e8.0046  Vlan167         
+ 
+        
         """
         
         self.verbose = 1
-        self.cfile = 'c:/arp_output'
+        self.path = os.getcwd() + '\\'
+        self.cfile = self.path +  'r++'
+        self.db_file = self.path + 'arp_db'
+        self.mping_load_file = '//an/produban/Systems/Comms/Network Operations\Software\Multi Ping 1 & 2\R++ load.txt'
         self.ip_index = {}
         self.mac_index = {}
         self.name_index = {}
         self.branch_list = []
         self.test_list = []    #just the devices in current test run
         self.test_ip = []
+        self.report_out = []
+        self.nexus = 0
         
         try:    #load the arp dict file
-            self.arp_file = open('c:/arp_db', 'rb')
+            self.arp_file = open(self.db_file)
             self.arp_dict = pickle.load(self.arp_file)
             self.arp_file.close()
         
         except: self.arp_dict = {}
         
         self.make_index()
+        
+        #search lists
+        #self.arp_ip_list = sorted(self.ip_index.keys())
+        #self.arp_mac_list = [x.split('_')[1] for x in self.mac_index.keys()]
     
     
     def search_mac(self, mac):
@@ -85,11 +98,16 @@ class Arp(Mnet, Mac):
         
         for row in reader:
             if row:
+                ip = ''
                 rows = row[0]
                 if self.verbose > 2: print rows
                 
-                if '#' in rows: branch = rows.split('#')[0]    #use router name as branch or create rules to id branch / office
+                if '#sh' in rows: branch = rows.split('#')[0]    #use router name as last resort
                 if '>' in rows: branch = rows.split('>')[0]    #
+                if 'san' in rows: branch = rows[4:9]           #san uk branch
+                if 'ab-h' in rows: branch = 'h' + rows[5:9]     #head office number
+                if 'alg' in rows: branch = 'h' + rows[4:8]    #ALG head office number
+                if 'bip' in rows: branch = 'b' + rows[4:8]       #BIP agency
                 
                 try: 
                     if branch: pass
@@ -101,14 +119,21 @@ class Arp(Mnet, Mac):
                 if branch not in self.branch_list: self.branch_list.append(branch)
                 if branch not in self.test_list: self.test_list.append(branch)
                 
-                if 'Internet' in rows and 'Incomplete' not in rows:    #cisco sh ip arp output
-                    
+                if 'Glean' in rows: self.nexus = 1
+                
+                if 'Internet' in rows and 'Incomplete' not in rows:
                     ip = rows[10:25].rstrip()
                     interface = rows[61:len(rows)]
                     mac = rows[38:52]
+                     
+                if self.nexus == 1 and '.' in rows:
+                    ip = rows.split()[0]
+                    interface = rows.split()[3]
+                    mac = rows.split()[2]
+                    
+                if ip:    
                     desc = self.classify(mac)
                     vendor = self.id_mac(mac)[0]
-                    
                     self.test_ip.append(ip)
                     
                     if interface not in self.arp_dict[branch]:
@@ -126,6 +151,10 @@ class Arp(Mnet, Mac):
                     self.arp_dict[branch][interface][ip_mac]['time'] = self.time
                     
                     if desc: self.arp_dict[branch][interface][ip_mac]['desc'] = desc
+                    
+                    if 'alarm' in desc or '.193' in ip: 
+                        port_res = self.check_port(ip, 80)[1:3]
+                        self.arp_dict[branch][interface][ip_mac]['port'] = port_res
                         
                     #update index dict's
                     self.ip_index[ip] = (branch, interface, ip_mac)
@@ -136,9 +165,11 @@ class Arp(Mnet, Mac):
         print '\n'
         
         
-    def classify(self, mac):    #clasify mac by user defined list 
+    def classify(self, mac):    #clasify mac by user defined list
+        if '0017.c5' in mac: return 'Alarm' 
         if '0021.b7' in mac: return 'Printer'
         if '0000.0c' in mac: return 'HSRP_Gw'
+        if '001a.d4' in mac: return 'ATM'
         return ''
      
      
@@ -161,31 +192,40 @@ class Arp(Mnet, Mac):
         
     
     def report(self, branch=''):
+        self.report_out = []
         if branch: branch_list = [branch]
         else: branch_list = self.test_list
         
         rep = ['ping', 'vendor', 'dns_name', 'port_scan']
         
-        print '\n IP Address      Branch        MAC         Intf   Delay   MAC Vendor   Desc / Hostname\n'
+        print '\n IP Address      Branch        MAC        Intf            Delay      Vendor    Hostname\n'
         ip_list = sorted(self.ip_index)
         for ip in ip_list:
             ind = self.ip_index[ip]
             res = self.arp_dict[ind[0]][ind[1]][ind[2]]
             
             if ind[0] in branch_list:
-                print '%s %s  %s  %s  %s ' % (ip, self.space(ip, 15), ind[0], ind[2].split('_')[1], ind[1]),
+                res_ping = res_vendor =  res_dns = res_port = ''
+                
                 for item in rep: 
                     try: 
-                        if 'ping' in item: print res[item], ' ',
-                        elif 'dns_name' in item: print res[item], self.space(item, 20),
-                        else: print res[item], self.space(item, 12),
+                        if 'ping' in item: res_ping = res[item]
+                        if 'vendor' in item: res_vendor = res[item]
+                        if 'dns_name' in item: res_dns = res[item]
+                        if 'port' in item: res_port = res[item]
                     except: pass
-                print
+                    
+                res_out = '%s%s  %s  %s  %s%s %s%s %s%s  %s%s %s ' % (ip, self.space(ip, 16), ind[0], ind[2].split('_')[1], ind[1], self.space(ind[1], 16), res_ping[:9], self.space(res_ping[:9], 10), res_vendor, self.space(res_vendor, 8), res_dns, self.space(res_dns, 20), res_port)
+                self.report_out.append(res_out)
+                print res_out
+                
+        
+    def report_mping(self): self.list_to_file(self.report_out, self.mping_load_file, mode='w')
                 
                 
     def save_dict(self):
         try:
-            self.arp_file = open('c:/arp_db', 'wb')
+            self.arp_file = open(self.db_file, 'wb')
             pickle.dump(self.arp_dict, self.arp_file)
             self.arp_file.close()
         
@@ -210,6 +250,11 @@ class Arp(Mnet, Mac):
                     except: pass
                     
                     if self.verbose > 2: print mac, branch, intf, ip, dns
+                    
+                    
+    def save_report(self):
+        q = raw_input('press s to save output to multi ping load file >>>')
+        if 's' in q.lower(): self.report_mping()
                 
             
     def arp_go(self):
@@ -220,13 +265,9 @@ class Arp(Mnet, Mac):
         self.save_dict()
         self.report()
         
-               
-       
+                
 if __name__ == "__main__":
     a = Arp()
     a.arp_go()
     end = raw_input('press enter to exit')
-                    
-
-                    
                     
